f950f431d67399cd15c6bde055c3982dcef17d31
Ekow <ewusiessel@gmail.com>
2021-12-17 13:35:36 +0000

    Modify index.js and index.css

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./main-component/App/App";
 import * as serviceWorker from "./serviceWorker";
-// import "./css/font-awesome.min.css";
-// import "./css/themify-icons.css";
-// import "./css/flaticon.css";
+import "./css/font-awesome.min.css";
+import "./css/themify-icons.css";
+import "./css/flaticon.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
