7fce6de676115ddc9f40affac3bea75577babcbe
Ekow <ewusiessel@gmail.com>
2022-03-08 07:04:46 +0000

    Update index.js

--- src/components/ContactFrom/index.js
+++ src/components/ContactFrom/index.js
@@ -74,70 +74,72 @@ class ContactForm extends Component {
     const { name, email, subject, lastname, error } = this.state;
 
     return (
-      <form onSubmit={this.subimtHandler} className="form">
-        <div className="row">
-          <div className="col-lg-6 col-md-6 col-12">
-            <div className="form-field">
-              <input
-                value={name}
-                onChange={this.changeHandler}
-                type="text"
-                name="name"
-                placeholder="Name"
-              />
-              <p>{error.name ? error.name : ""}</p>
+      <div className="container">
+        <form onSubmit={this.subimtHandler} className="form">
+          <div className="row justify-content-center">
+            <div className="col-lg-6 col-md-6 col-12">
+              <div className="form-field">
+                <input
+                  value={name}
+                  onChange={this.changeHandler}
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                />
+                <p>{error.name ? error.name : ""}</p>
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-6 col-12">
+              <div className="form-field">
+                <input
+                  value={lastname}
+                  onChange={this.changeHandler}
+                  type="text"
+                  name="lastname"
+                  placeholder="Lastname"
+                />
+                <p>{error.lastname ? error.lastname : ""}</p>
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-6 col-12">
+              <div className="form-field">
+                <input
+                  onChange={this.changeHandler}
+                  value={email}
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                />
+                <p>{error.email ? error.email : ""}</p>
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-6 col-12">
+              <div className="form-field">
+                <input
+                  onChange={this.changeHandler}
+                  value={subject}
+                  type="text"
+                  name="subject"
+                  placeholder="Subject"
+                />
+                <p>{error.subject ? error.subject : ""}</p>
+              </div>
+            </div>
+            <div className="col-lg-12">
+              <div className="form-field">
+                <textarea name="message" placeholder="Message"></textarea>
+              </div>
+            </div>
+            <div className="col-lg-12">
+              <div className="form-submit">
+                <button type="submit" className="theme-btn">
+                  Send Message
+                </button>
+              </div>
             </div>
           </div>
-          <div className="col-lg-6 col-md-6 col-12">
-            <div className="form-field">
-              <input
-                value={lastname}
-                onChange={this.changeHandler}
-                type="text"
-                name="lastname"
-                placeholder="Lastname"
-              />
-              <p>{error.lastname ? error.lastname : ""}</p>
-            </div>
-          </div>
-          <div className="col-lg-6 col-md-6 col-12">
-            <div className="form-field">
-              <input
-                onChange={this.changeHandler}
-                value={email}
-                type="email"
-                name="email"
-                placeholder="Email"
-              />
-              <p>{error.email ? error.email : ""}</p>
-            </div>
-          </div>
-          <div className="col-lg-6 col-md-6 col-12">
-            <div className="form-field">
-              <input
-                onChange={this.changeHandler}
-                value={subject}
-                type="text"
-                name="subject"
-                placeholder="Subject"
-              />
-              <p>{error.subject ? error.subject : ""}</p>
-            </div>
-          </div>
-          <div className="col-lg-12">
-            <div className="form-field">
-              <textarea name="message" placeholder="Message"></textarea>
-            </div>
-          </div>
-          <div className="col-lg-12">
-            <div className="form-submit">
-              <button type="submit" className="theme-btn">
-                Send Message
-              </button>
-            </div>
-          </div>
-        </div>
-      </form>
+        </form>
+      </div>
     );
   }
 }
