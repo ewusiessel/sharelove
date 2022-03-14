49886a850e07080f49d7de137ff53811f0b965c4
Ekow <ewusiessel@gmail.com>
2022-03-14 21:35:56 +0000

    Remove background image from modal

--- src/main-component/ForgotPassword/index.js
+++ src/main-component/ForgotPassword/index.js
@@ -5,7 +5,6 @@ import { toast } from "react-toastify";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import { Link, withRouter } from "react-router-dom";
-import s1 from "../../images/shape.png";
 
 const ForgotPassword = (props) => {
   const [value, setValue] = useState({
